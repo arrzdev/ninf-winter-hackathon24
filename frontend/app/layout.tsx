27bcfb34b81e040d4070ff4241343264b7217ace
arrzdev <andre.arroz.santos@gmail.com>
2024-02-18 13:53:36 +0000

    Added app metadata

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -2,46 +2,46 @@ import './globals.css'
 import type { Metadata, Viewport } from 'next'
 import NavBar from '@/lib/components/nav-bar'
 
-const APP_NAME = "PWA App";
-const APP_DEFAULT_TITLE = "My Awesome PWA App";
-const APP_TITLE_TEMPLATE = "%s - PWA App";
-const APP_DESCRIPTION = "Best PWA app in the world!";
+const APP_NAME = "COMPARA";
+const APP_DEFAULT_TITLE = "COMPARA";
+const APP_TITLE_TEMPLATE = "%s - COMPARA";
+const APP_DESCRIPTION = "Compara preços entre diferentes supermercados e encontra o melhor preço para os teus produtos favoritos.";
 
-// export const metadata: Metadata = {
-//   applicationName: APP_NAME,
-//   title: {
-//     default: APP_DEFAULT_TITLE,
-//     template: APP_TITLE_TEMPLATE,
-//   },
-//   description: APP_DESCRIPTION,
-//   manifest: "/manifest.json",
-//   appleWebApp: {
-//     capable: true,
-//     statusBarStyle: "default",
-//     title: APP_DEFAULT_TITLE,
-//     // startUpImage: [],
-//   },
-//   formatDetection: {
-//     telephone: false,
-//   },
-//   openGraph: {
-//     type: "website",
-//     siteName: APP_NAME,
-//     title: {
-//       default: APP_DEFAULT_TITLE,
-//       template: APP_TITLE_TEMPLATE,
-//     },
-//     description: APP_DESCRIPTION,
-//   },
-//   twitter: {
-//     card: "summary",
-//     title: {
-//       default: APP_DEFAULT_TITLE,
-//       template: APP_TITLE_TEMPLATE,
-//     },
-//     description: APP_DESCRIPTION,
-//   },
-// };
+export const metadata: Metadata = {
+  applicationName: APP_NAME,
+  title: {
+    default: APP_DEFAULT_TITLE,
+    template: APP_TITLE_TEMPLATE,
+  },
+  description: APP_DESCRIPTION,
+  manifest: "/manifest.json",
+  appleWebApp: {
+    capable: true,
+    statusBarStyle: "default",
+    title: APP_DEFAULT_TITLE,
+    // startUpImage: [],
+  },
+  formatDetection: {
+    telephone: false,
+  },
+  openGraph: {
+    type: "website",
+    siteName: APP_NAME,
+    title: {
+      default: APP_DEFAULT_TITLE,
+      template: APP_TITLE_TEMPLATE,
+    },
+    description: APP_DESCRIPTION,
+  },
+  twitter: {
+    card: "summary",
+    title: {
+      default: APP_DEFAULT_TITLE,
+      template: APP_TITLE_TEMPLATE,
+    },
+    description: APP_DESCRIPTION,
+  },
+};
 
 export const viewport: Viewport = {
   width: "device-width",
