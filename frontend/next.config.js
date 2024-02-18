49fb6fa0c2b99a48251ac54338920335f8a6bb80
arrzdev <andre.arroz.santos@gmail.com>
2024-02-18 03:06:00 +0000

    Added next experimental config bc of a bug in Suspend

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -9,6 +9,9 @@ const withPWA = require("next-pwa")({
 
 const nextConfig = withPWA({
   reactStrictMode: true,
+  experimental: {
+    missingSuspenseWithCSRBailout: false,
+  },
   images: {
     remotePatterns: [{
       protocol: "https",
